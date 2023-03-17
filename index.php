8410f1e7a8713bba8b99b728f8d41bdeedde8e39
Research Actor <aslanatemov@gmail.com>
2023-03-17 15:18:53 +0300

    sync

--- index.php
+++ index.php
@@ -1,20 +1,50 @@
 <?php
-// Дерево документа
-echo $_SERVER['DOCUMENT_ROOT']."<br>";
-// Некие параметры относительно типа документа
-echo $_SERVER['HTTP_ACCEPT']."<br>";
-// Предпочтения клиента касательно языка
-echo $_SERVER['HTTP_ACCEPT_LANGUAGE']."<br>";
-// Получение хоста
-echo $_SERVER['HTTP_HOST']."<br>";
-// Ссылка откуда мы пришли на страницу
-echo $_SERVER['HTTP_REFERER']."<br>";
-// Тип браузера и операционной системы
-echo $_SERVER['HTTP_USER_AGENT']."<br>";
-// Возвращается IP-адрес клиента
-echo $_SERVER['REMOTE_ADDR']."<br>";
-// Абсолютный путь исполняемого файла
-echo $_SERVER['SCRIPT_FILENAME']."<br>";
-// 
+// Методы
 
+class Hi {
+    public function hello () {
+        return 'Hello everybody';
+    }
+
+    public function bye () {
+        return 'Bye';
+    }
+}
+
+$obj = new Hi;
+
+echo $obj->hello() . "<br>";
+echo $obj->bye() . "<br>";
+
+// var_dump($obj);
+
+class Family {
+    private $boy;
+    private $girl;
+
+    public function setNameBoy($boy) {
+        $this->boy = $boy;
+    }
+
+    public function setNameGirl($girl) {
+        $this->girl = $girl;
+    }
+    public function getNameBoy(){
+        return $this->boy;
+    }
+    public function getNameGirl(){
+        return $this->boy;
+    }
+
+    public function family(){
+        return $this->getNameGirl() . " и " . $this->getNameBoy() . " семья!";
+    }
+}
+
+$family = new Family;
+
+$family->setNameBoy('Ник');
+$family->setNameGirl('Нина');
+
+echo $family->family();
 ?>
